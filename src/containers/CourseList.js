4f19d59284b381c39845f9d7c9f9ebfcf4740331
Dhruv <dhruvdev7794@gmail.com>
2018-05-19 18:37:55 -0400

    added COurseList and CourseRow

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import CourseRow from "../component/CourseRow";
 class CourseList extends React.Component {
    constructor() {
        super();
    }
    render() {
        return (
-           <h2>Course List</h2>
+           <div>
+             <h2>Course List</h2>
+             <table>
+               <thead>
+               <tr>
+                   <th>Title</th>
+               </tr>
+               </thead>
+               <tbody>
+                    <CourseRow/>
+               </tbody>
+             </table>
+           </div>
        )
    }
 }
