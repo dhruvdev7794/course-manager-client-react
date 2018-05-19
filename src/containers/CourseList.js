9e3cbd331177363c6a0b4a194485b2fcfea5b996
Dhruv <dhruvdev7794@gmail.com>
2018-05-19 18:51:16 -0400

    made get courses

--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import CourseRow from "../component/CourseRow";
+import CourseService from '../services/CourseService'
 class CourseList extends React.Component {
    constructor() {
+
        super();
+       this.courseService = CourseService.instance;
+       this.state = {courses: []};
    }
-   render() {
+
+    componentDidMount() {
+        this.courseService.findAllCourses()
+            .then((courses) => {
+                console.log(courses)
+                this.setState({courses: courses});
+            });
+    }
+
+    courseRow(){
+       let courses = this.state.courses.map(function (course) {
+           return <CourseRow key={course.id} course = {course}/>;
+       })
+        return (
+            courses
+        )
+    }
+    render() {
        return (
            <div>
              <h2>Course List</h2>
@@ -13,9 +34,14 @@ class CourseList extends React.Component {
                <tr>
                    <th>Title</th>
                </tr>
+               <tr>
+                   <th><input id="titleFld"
+                              placeholder="cs101"/></th>
+                   <th><button>Add</button></th>
+               </tr>
                </thead>
                <tbody>
-                    <CourseRow/>
+               {this.courseRow()}
                </tbody>
              </table>
            </div>
