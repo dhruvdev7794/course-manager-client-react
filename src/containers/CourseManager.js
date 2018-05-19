8129c54856bca41262ec9d62adcbd603215a3528
Dhruv <dhruvdev7794@gmail.com>
2018-05-19 19:46:10 -0400

    made routes and added clickable link

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import CourseList from './CourseList';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
 
 class CourseManager extends Component {
     render(){
         return(
-            <div className = "container-fluid">
-                <h1>Course Manager</h1>
-                <CourseList/>
-            </div>
+            <Router>
+                <div className = "container-fluid">
+                    <h1>Course Manager</h1>
+                    <Route path = "/courses"
+                            component={CourseList}>
+                    </Route>
+                </div>
+            </Router>
         )
     }
 }
