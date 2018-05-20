f81ef6c84bd137de6b18a7d5e993bf99ea4fc3c0
Dhruv <dhruvdev7794@gmail.com>
2018-05-20 15:12:04 -0400

    module list now displaying

--- src/containers/ModuleList.js
+++ src/containers/ModuleList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ModuleListItem from '../component/ModuleListItem';
+import ModuleService from '../services/ModuleService';
 
 export default class ModuleList
     extends React.Component {
@@ -7,35 +8,51 @@ export default class ModuleList
         super(props);
         this.state = {
             module: {title:""},
-            courseId: '',
-            modules: [
-                {title: 'Module 1 - jQuery', id: 123},
-                {title: 'Module 2 - React', id: 234},
-                {title: 'Module 3 - Redux', id: 345},
-                {title: 'Module 4 - Angular', id: 456},
-                {title: 'Module 5 - Node.js', id: 567},
-                {title: 'Module 6 - MongoDB', id: 678}
-            ]
+            courseId: ''
+            // modules: [
+            //     {title: 'Module 1 - jQuery', id: 123},
+            //     {title: 'Module 2 - React', id: 234},
+            //     {title: 'Module 3 - Redux', id: 345},
+            //     {title: 'Module 4 - Angular', id: 456},
+            //     {title: 'Module 5 - Node.js', id: 567},
+            //     {title: 'Module 6 - MongoDB', id: 678}
+            // ]
         };
         this.setCourseId = this.setCourseId.bind(this);
         this.titleChanged = this.titleChanged.bind(this);
         this.createModule = this.createModule.bind(this);
+        this.moduleService = ModuleService.instance;
+    }
+    setModules(modules) {
+        this.setState({modules: modules})
     }
     setCourseId(courseId) {
         this.setState({courseId: courseId});
     }
+
     componentDidMount() {
         this.setCourseId(this.props.courseId);
     }
+    findAllModulesForCourse(courseId) {
+        this.moduleService
+            .findAllModulesForCourse(courseId)
+            .then((modules) => {this.setModules(modules)});
+    }
+
     componentWillReceiveProps(newProps){
         this.setCourseId(newProps.courseId);
+        this.findAllModulesForCourse(newProps.courseId)
+
     }
 
     renderListOfModules(){
-        var modules = this.state.modules.map((module) => {
-            return <ModuleListItem key = {module.id} title = {module.title}  courseId = {this.state.courseId}/>
-        });
-        return modules;
+        if(this.state.modules!=undefined){
+            var modules = this.state.modules.map((module) => {
+                return <ModuleListItem key = {module.id} title = {module.title}  courseId = {this.state.courseId}/>
+            });
+            return modules;
+        }
+        return null;
     }
     titleChanged(event) {
         console.log(event.target.value);
@@ -43,7 +60,8 @@ export default class ModuleList
     }
 
     createModule(event){
-        console.log(this.state.module)
+        console.log(this.state.module);
+        this.moduleService.createModule(this.props.courseId, this.state.module);
     }
     render(){
         return(
