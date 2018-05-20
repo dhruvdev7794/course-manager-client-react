eef60c8931894bb057363d9f3064ebf30d7ae9ff
Dhruv <dhruvdev7794@gmail.com>
2018-05-19 20:01:53 -0400

    editted courseEditor to get the id

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -3,14 +3,33 @@ import ModuleList from './ModuleList'
 import LessonTab from './LessonTab'
 
 export default class CourseEditor extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {courseId: ''};
+        this.selectCourse = this.selectCourse.bind(this);
+
+    }
+
+    componentDidMount() {
+       this.selectCourse(this.props.match.params.courseId);
+    }
+
+
+    selectCourse(courseId) {
+       this.setState({courseId: courseId});
+    }
     render(){
         return(
-            <div className="row">
-                <div className="col-4">
-                    <ModuleList/>
-                </div>
-                <div className="col-8">
-                    <LessonTab/>
+            <div>
+                <h3>Course {this.state.courseId}</h3>
+                <div className="row">
+                    <div className="col-4">
+                        <ModuleList/>
+                    </div>
+                    <div className="col-8">
+                        <LessonTab/>
+                    </div>
                 </div>
             </div>
         );
