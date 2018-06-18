5d309885240007da7a26491802650b3a76cc7def
Dhruv <dhruvdev7794@gmail.com>
2018-06-18 10:47:42 -0400

    made changes to widget and reducer

--- src/component/widget.js
+++ src/component/widget.js
@@ -1,15 +1,65 @@
 import React from 'react';
-import {DELETE_WIDGET} from "../constants";
+import {DELETE_WIDGET, SELECT_WIDGET_TYPE} from "../constants";
 import {connect} from 'react-redux';
 
-const Widget = ({widget, dispatch}) => (
-    <li key={widget.id}>
-        {widget.text}
-        <button onClick={e => (
-            dispatch({type:DELETE_WIDGET, id: widget.id})
-        )}> Delete </button>
-    </li>
+const Heading = () => (
+    <h1>Heading</h1>
 );
+
+const Paragraph = () => (
+    <h1>Paragraph</h1>
+);
+
+const List = () => (
+    <h1>List</h1>
+);
+
+const Image = () => (
+    <h1>Image</h1>
+);
+
+const Link = () => (
+    <h1>Link</h1>
+);
+
+
+const Widget = ({widget, dispatch}) => {
+    let selectElement;
+
+    return(
+        <li key={widget.id}>
+            {widget.text}
+            {widget.widgetType}
+
+
+            <div>
+                {widget.widgetType === "Heading" && <Heading/>}
+                {widget.widgetType === "Paragraph" && <Paragraph/>}
+                {widget.widgetType === "Image" && <Image/>}
+                {widget.widgetType === "List" && <List/>}
+                {widget.widgetType === "Link" && <Link/>}
+            </div>
+
+
+            <select onChange={event => dispatch({
+                type: SELECT_WIDGET_TYPE,
+                id:widget.id,
+                widgetType: selectElement.value
+            })} ref={node => selectElement = node} className="float-right">
+                <option>Heading</option>
+                <option>Paragraph</option>
+                <option>List</option>
+                <option>Image</option>
+                <option>Link</option>
+            </select>
+
+
+            <button onClick={e => (
+                dispatch({type:DELETE_WIDGET, id: widget.id})
+            )}> Delete </button>
+        </li>
+    )
+};
 
 export const WidgetContainer = connect(state => ({
     lessonId: state.lessonId
--- src/reducers/widgetReducer.js
+++ src/reducers/widgetReducer.js
@@ -1,8 +1,19 @@
-import {ADD_WIDGET, DELETE_WIDGET, FIND_ALL_WIDGETS, SAVE} from "../constants";
+import {ADD_WIDGET, DELETE_WIDGET, FIND_ALL_WIDGETS, SAVE, SELECT_WIDGET_TYPE} from "../constants";
 
 let autoIncroment = 3;
 export const widgetReducer = (state = {widgets: []}, action) => {
     switch (action.type){
+        case SELECT_WIDGET_TYPE:
+            console.log(action);
+            let newState = {
+                widgets: state.widgets.filter((widget)=>{
+                    if(widget.id === action.id){
+                        widget.widgetType = action.widgetType;
+                    }
+                    return true;
+                })
+            };
+            return JSON.parse(JSON.stringify(newState));
         case SAVE:
             fetch("http://localhost:8080/api/widget/save",{
                 method: 'post',
@@ -25,7 +36,8 @@ export const widgetReducer = (state = {widgets: []}, action) => {
             return {
                 widgets:[
                     ...state.widgets,
-                    { id: autoIncroment++, text: 'New widget' }
+                    { id: autoIncroment++, text: 'New widget',
+                    widgetType: "Paragraph"}
                 ]
             };
         default:
